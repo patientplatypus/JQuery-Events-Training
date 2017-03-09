82655514a37492504f9284474c69fdf1feaa2cb2
patientplatypus <pweyand@gmail.com>
2017-03-08 23:35:15 -0600

    done

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -1,7 +1,90 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+$(document).ready(function(evt){
 
   $("#time").text( Date.now() );
+  console.log("event",evt);
+
+  function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  	var leftvalue = 0;
+  	var rightvalue = 0;
+  	var random = getRandomArbitrary(1,101);
+  	var total = random;
+  	var change = false;
+
+	var s = $('<input type="number" class="add" id="random">');
+	$("#time").after(s);
+
+	$("#random").val(random);
+	$("#total").val(random);
+	$("#right").val(rightvalue);
+	$("#left").val(leftvalue)
+
+
+	$( "#right" ).on("input",function() {
+		  leftvalue = $('#left').val();
+		  rightvalue = $('#right').val();
+		  total = parseFloat(leftvalue) + parseFloat(rightvalue) + parseFloat(random);
+		  $('#total').val(total);
+	});
+
+	$( "#left" ).on("input",function() {
+		  leftvalue = $('#left').val();
+		  rightvalue = $('#right').val();
+		  total = parseFloat(leftvalue) + parseFloat(rightvalue) + parseFloat(random);
+		  $('#total').val(total);
+	});
+
+
+	setInterval(function (){
+		console.log("total", total);
+		if (total != 0){
+			var resetbox = $("body").find("#reset");
+			var htmlbody = $("body");
+
+			if (resetbox.length > 0){
+				$("#reset").click(function(){
+					random = getRandomArbitrary(1,101);
+					$("#left").val(0);	
+					$("#right").val(0);
+					$("#random").val(random);
+					$("#total").val(random);
+				});
+			}else{
+				var r = $('<input type="button" value="RESET" id="reset"/>');
+	       		$("body").append(r);
+			}
+		}
+	},500);
+
+
+//	$('#add').on('change',function(){
+//		alert("hey");
+//	})
+
+
+//	$('#total').on("change", function(){
+//		alert("hey");
+//	});
+
+
+//	if (change===true){
+//		if (total!=0){
+//			 var r= $('<input type="button" value="new button"/>');
+//       		 $("body").append(r);
+//		}
+//		change=false;
+//	}
+
+
+ // evt.keydown(function(){
+
+	 
+	//})
+
+
 
 })
--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -2,4 +2,44 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+	//var assignment
+
+
+	$("span").eq(0).click(function(){
+		r =$('<li>Fire the missiles!</li>') 
+		$('ul').append(r);
+	});
+	
+	$("span").eq(1).click(function(){
+		r =$('<li>Launch the armada!</li>') 
+		$('ul').append(r);
+	});
+	
+	$("span").eq(2).click(function(){
+		r =$('<li>Charge!</li>') 
+		$('ul').append(r);
+	});
+	
+	$("span").eq(3).click(function(){
+		r =$('<li>Exterminate!</li>') 
+		$('ul').append(r);
+	});
+	
+	$("span").eq(4).click(function(){
+		r =$('<li>Into the breech!</li>') 
+		$('ul').append(r);
+	});
+	
+	$("span").eq(5).click(function(){
+		r =$('<li>This is Sparta!</li>') 
+		$('ul').append(r);
+	});
+	
+	$("span").eq(6).click(function(){
+		r =$('<li>Huzzah!</li>') 
+		$('ul').append(r);
+	});
+	
+
+
 })
